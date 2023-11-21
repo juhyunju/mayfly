821f6b333213a2ee694802f08c47689834a04a56
juhyunju <2juhyunju@naver.com>
2023-11-21 18:15:50 +0900

    feat:chatDone

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const chatRoomRouter = require("./chatRoomRouter")
+const messageRouter = require("./messageRouter")
+router.use('/chat',chatRoomRouter)
+router.use('/message',messageRouter)
 
 
 module.exports = router;
