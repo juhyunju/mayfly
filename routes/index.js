85fc1d4c52c5dec16343c1d8e1bfd31e8dfc307c
beomseok_wecode <beomseok_wecode@MacBook-Air.local>
2023-11-21 18:41:13 +0900

    끝이 보입니다

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-  
 const adminRouter = require("./adminRouter");
 const userRouter = require("./userRouter");
 const hostRouter = require("./hostRouter");
 const classesRouter = require("./classesRouter");
 const schedulesRouter = require("./schedulesRouter");
-const imageRouter = require("./imageRouter")
-const categoryRouter = require("./categoryRouter")
-const chatRoomRouter = require("./chatRoomRouter")
-const messageRouter = require("./messageRouter")
+const imageRouter = require("./imageRouter");
+const categoryRouter = require("./categoryRouter");
+const chatRoomRouter = require("./chatRoomRouter");
+const messageRouter = require("./messageRouter");
+const likeRouter = require("./likeRouter");
+const orderRouter = require("./orderRouter");
 
 router.use("/admins", adminRouter.router);
 router.use("/users", userRouter.router);
 router.use("/hosts", hostRouter.router);
-router.use('/classes',classesRouter.router);
-router.use('/schedules',schedulesRouter.router);
-router.use('/categories',categoryRouter)
-router.use('/images',imageRouter)
-router.use('/chat',chatRoomRouter)
-router.use('/message',messageRouter)
+router.use("/classes", classesRouter.router);
+router.use("/schedules", schedulesRouter.router);
+router.use("/categories", categoryRouter);
+router.use("/images", imageRouter);
+router.use("/chat", chatRoomRouter);
+router.use("/message", messageRouter);
+router.use("/likes", likeRouter);
+router.use("/orders", orderRouter);
 
 module.exports = router;
-
-
