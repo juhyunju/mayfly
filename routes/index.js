5b19d02712976029824baf9ddcb2a020a3407a01
juhyunju <2juhyunju@naver.com>
2023-11-21 17:39:52 +0900

    feat:images

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const imageRouter = require("./imageRouter")
+router.use('/images',imageRouter)
+
 
 module.exports = router;
