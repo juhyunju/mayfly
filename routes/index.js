521aaeb92bd658488420bb12bb0eab1f670b9d76
juhyunju <2juhyunju@naver.com>
2023-11-21 18:02:40 +0900

    feat:categories

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
+const categoryRouter = require("./categoryRouter")
+router.use('/categories',categoryRouter)
 module.exports = router;
